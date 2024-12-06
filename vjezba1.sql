100477b2421b9e4fde648f773d507b5698507168
bbasic031 <53994765+bbasic031@users.noreply.github.com>
2024-12-06 20:57:02 +0100

    6/12 final update

---
+++ vjezba1.sql
@@ -0,0 +1,31 @@
+﻿use master;
+go
+drop database if exists vjezba1;
+go
+create database vjezba1 collate Croatian_CI_AS;
+go
+use vjezba1;
+go
+
+create table IspitniRok(
+sifra int NOT NULL primary key identity(1,1), -- ovo je primarni ključ
+predmet varchar(50) NOT NULL,
+vrstaIspita varchar(50) NOT NULL,
+datum datetime,
+pristupio int NOT NULL
+);
+
+create table Pristupnici(
+ispitniRok int not null REFERENCES IspitniRok(sifra),
+student int not null,
+brojbodova int not null,
+ocjena int not null
+);
+
+SELECT * from IspitniRok
+
+INSERT INTO IspitniRok(predmet,vrstaIspita,pristupio) VALUES
+('Programiranje','Pismeni',23),
+('Baze podataka','Usmeni',17);
+
+DELETE FROM IspitniRok
