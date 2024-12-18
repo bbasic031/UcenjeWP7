66cdab3ba76a393450fd45898f6b590ad691620d
bbasic031 <53994765+bbasic031@users.noreply.github.com>
2024-12-18 20:53:25 +0100

    Osnove C# 18/12

---
+++ CSharp/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+        public static void Izvedi()
+        {
+            //Output
+            Console.WriteLine("Pozdrav svijetu!");
+
+            Console.Write("Ovo je u liniji ");
+
+            Console.WriteLine("ovo je u istoj liniji, ali nakon ispisa ode u novu liniju");
+
+            Console.WriteLine("1. red\n2.red\t nakon taba");
+
+            Console.WriteLine(1);
+            Console.WriteLine(true);
+            Console.WriteLine(4.6);
+
+
+            //Input
+            int i;
+            Console.Write("Unesi cijeli broj: ");
+            i = int.Parse(Console.ReadLine());
+
+            //Output
+            Console.WriteLine("Unio si {0}",i);
+
+
+            //Input
+            Console.Write("Unesi ime grada: ");
+            string grad=Console.ReadLine();
+
+            //Output
+            Console.WriteLine("Unio si "+ grad);
+
+
+
+        }
+    }
+}
---
+++ CSharp/Ucenje/E03Operatori.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E03Operatori
+    {
+        public static void Izvedi()
+        {
+            int i, j;
+            i = 2; j = 3;
+            i += j;
+            Console.WriteLine(i);
+
+            Console.WriteLine(i/j);
+            Console.WriteLine(i/(float)j);
+            Console.WriteLine(i%j);
+        }
+    }
+}
